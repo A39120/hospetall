91c1de46ecd5914f49c1627bfd8a762597b9a757
A39120 <lariachi@gmail.com>
2018-04-12 20:57:56 +0100

    Minor changes in database tables

--- database/tables.sql
+++ database/tables.sql
@@ -1,34 +1,51 @@
+USE HosPetAll;
+
+IF OBJECT_ID('dbo.Pet') IS NOT NULL 
+	DROP TABLE dbo.Pet; 
+  
+IF OBJECT_ID('dbo.Race') IS NOT NULL
+	DROP TABLE dbo.Race
+
+IF OBJECT_ID('dbo.Species') IS NOT NULL
+	DROP TABLE dbo.Species
+
+IF OBJECT_ID('dbo.UserClient') IS NOT NULL 
+	DROP TABLE dbo.UserClient; 
+
+IF OBJECT_ID('dbo.Client') IS NOT NULL
+	DROP TABLE dbo.Client
 
 CREATE TABLE Client(
-	id INTEGER PRIMARY KEY IDENTITY(1,1), 
-	given_name VARCHAR(256) NOT NULL,
-	family_name VARCHAR(256) NOT NULL,
-	address VARCHAR(1024) UNIQUE,
-	postal_code VARCHAR(16), 
-	telephone INT UNIQUE, 
-	telephone_alternative INT,
-	email VARCHAR(256) UNIQUE, 
-	-- nif INT 
-	CONSTRAINT ct_mphone CHECK (telephone > 0 AND telephone <= 999999999)
+	Id INTEGER PRIMARY KEY IDENTITY(1,1), 
+	GivenName VARCHAR(256) NOT NULL,
+	FamilyName VARCHAR(256) NOT NULL,
+	Address VARCHAR(1024) UNIQUE,
+	PostalCode VARCHAR(16), 
+	Telephone VARCHAR(12) UNIQUE, 
+	TelephoneAlternative VARCHAR(12),
+	Email VARCHAR(256) UNIQUE, 
+	Nif INT, 
+	CONSTRAINT ct_mphone CHECK (Telephone > 0 AND Telephone <= 999999999)
 )
 
+-- Application User
 CREATE TABLE UserClient(
-	id INTEGER REFERENCES Client(id), 
-	username VARCHAR(256) UNIQUE, 
-	register_date DATE NOT NULL,
-	hashpwd VARCHAR(512) NOT NULL, 
-	client_email VARCHAR(256) REFERENCES Client(email)
+	Id INTEGER REFERENCES Client(Id), 
+	Username VARCHAR(256) UNIQUE, 
+	RegisterDate DATE NOT NULL,
+--	Hashpwd VARCHAR(512) NOT NULL, 
+	ClientEmail VARCHAR(256) REFERENCES Client(Email) NOT NULL
 )
 
 CREATE TABLE Species(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY, 
-	name VARCHAR(31)
+	Id INTEGER IDENTITY(1,1) PRIMARY KEY, 
+	Name VARCHAR(31)
 )
 
 CREATE TABLE Race(
-	species_id INTEGER REFERENCES Species(id),
-	race_id INTEGER IDENTITY(1,1) UNIQUE NOT NULL,
-	name VARCHAR(31) UNIQUE
+	Species INTEGER REFERENCES Species(Id),
+	Id INTEGER IDENTITY(1,1) UNIQUE NOT NULL,
+	Name VARCHAR(31) UNIQUE
 )
 
 -- Check http://www.lpda.pt/legislacao/#Estatuto%20jur%C3%ADdico%20dos%20animais
@@ -36,9 +53,37 @@ CREATE TABLE Race(
 -- https://www.sira.com.pt/
 -- 
 CREATE TABLE Pet(
-	owner_id INTEGER REFERENCES Client(id),
-	p_id INTEGER IDENTITY(1,1) UNIQUE NOT NULL,
-	p_name VARCHAR(255) NOT NULL,
-	race_id INTEGER REFERENCES Race(id)
-	birth_date DATE -- Importante para a vacinação de algumas especies	
+	Owner INTEGER REFERENCES Client(Id),
+	Id INTEGER IDENTITY(1,1) UNIQUE NOT NULL,
+	Name VARCHAR(255) NOT NULL,
+	Race INTEGER REFERENCES Race(Id),
+	Species INTEGER REFERENCES Species(Id) NOT NULL,
+	Birthdate DATE, -- Importante para a vacinação de algumas especies	
+	ChipNumber INT, 
+	LicenseNumber INT
+)
+
+CREATE TABLE MedicalProcedure(
+	Id INTEGER IDENTITY(1,1) PRIMARY KEY,
+	Pet REFERENCES Pet(Id) NOT NULL, 
+	CaseHistory TEXT, 
+	Diagnosis TEXT, 
+	Treatment TEXT,
+	Observations TEXT
+)
+
+CREATE TABLE Veterinarian(
+	Id INTEGER IDENTITY(1,1) PRIMARY KEY, 
+	Name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE Nurse(
+	Id INTEGER IDENTITY(1,1) PRIMARY KEY, 
+	Name VARCHAR(255) NOT NULL 
+)
+
+CREATE TABLE Consultation(
+	Id INTEGER REFERENCES MedicalProcedure(Id) PRIMARY KEY, 
+	VeterinarianId REFERENCES Veterinarian(Id) NOT NULL, 
+	--Weight FLOAT, 
 )
