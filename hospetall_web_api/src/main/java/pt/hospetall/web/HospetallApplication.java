40c0380f44d3b8aa834e34f874123280000a9897
A39120 <lariachi@gmail.com>
2018-04-25 20:00:38 +0000

    Created new Web API project

---
+++ hospetall_web_api/src/main/java/pt/hospetall/web/HospetallApplication.java
@@ -0,0 +1,12 @@
+package pt.hospetall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospetallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospetallApplication.class, args);
+	}
+}
