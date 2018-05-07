b4ee0c6b38c3761d1bf4e8f9cd9a7cc13556f78c
A39120 <lariachi@gmail.com>
2018-05-07 00:56:15 +0000

    Created new insertion scripts for database

--- database/initdb.sh
+++ database/initdb.sh
@@ -2,11 +2,8 @@
 user=worker
 server=localhost
 password=1234567890
-ins=_insert.sql
 PATH="$HOME/LI61D-LI61N-G35/database"
 
-/bin/sqlcmd -S $server -U $user -P $password -i $PATH/person$ins
-/bin/sqlcmd -S $server -U $user -P $password -i $PATH/client$ins
-/bin/sqlcmd -S $server -U $user -P $password -i $PATH/nurse$ins
-/bin/sqlcmd -S $server -U $user -P $password -i $PATH/vet$ins
-
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/client.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/veterinarian.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/nurse.sql
