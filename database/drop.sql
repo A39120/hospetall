7331784957958a2f709eeb8b7fc855f9d33fcab7
A39120 <lariachi@gmail.com>
2018-05-07 21:15:31 +0000

    Created new insert scripts (consultation) for database testing, fixed race and pet scripts

---
+++ database/drop.sql
@@ -0,0 +1,10 @@
+USE HosPetAll;
+
+DROP TABLE pet
+DROP TABLE race
+DROP TABLE species
+DROP TABLE veterinarian
+DROP TABLE nurse
+DROP TABLE account
+DROP TABLE client
+DROP TABLE product 
--- database/initdb.sh
+++ database/initdb.sh
@@ -7,3 +7,7 @@ PATH="$HOME/LI61D-LI61N-G35/database"
 /bin/sqlcmd -S $server -U $user -P $password -i $PATH/client.sql
 /bin/sqlcmd -S $server -U $user -P $password -i $PATH/veterinarian.sql
 /bin/sqlcmd -S $server -U $user -P $password -i $PATH/nurse.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/species.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/race.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/pet.sql
+/bin/sqlcmd -S $server -U $user -P $password -i $PATH/consultation.sql
