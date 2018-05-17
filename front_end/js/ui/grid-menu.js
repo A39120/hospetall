2af05c8d40c93b98963ae0dd38549f802fb20d66
MiguelNogueira <a39202@alunos.isel.pt>
2018-05-17 22:09:28 +0100

    Change className types in react components

--- front_end/js/ui/grid-menu.js
+++ front_end/js/ui/grid-menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Menu from './menu'
+import DetailedPage from './detailed-page'
 
 export default class extends React.Component {
     constructor (props) {
@@ -13,8 +14,8 @@ export default class extends React.Component {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-2"><Menu parts={this.props.parts}/></div>
-                    <div className="col-lg-10"><p>Componente Principal</p></div>
+                    <div className="col-md-2"><Menu parts={this.props.parts}/></div>
+                    <div className="col-md-10"><DetailedPage /></div>
                 </div>
             </div>
         )
