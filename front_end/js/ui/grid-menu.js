2a8a8480d7f0f70f6fc678439d3e38f54d971f07
MiguelNogueira <a39202@alunos.isel.pt>
2018-05-20 04:07:05 +0100

    Add iteration between menu and detailed-page components

--- front_end/js/ui/grid-menu.js
+++ front_end/js/ui/grid-menu.js
@@ -2,20 +2,37 @@ import React from 'react'
 import Menu from './menu'
 import DetailedPage from './detailed-page'
 
+const FetchStates = [
+    {state: 'waiting-room', value: 'Salas de Espera'},
+    {state: 'appointment', value: 'Marcações'},
+    {state: 'client', value: 'Cliente'},
+    {state: 'pet', value: 'Animal'},
+    {state: 'product', value: 'Produtos'},
+    {state: 'configs', value: 'Configurações'}
+]
+
 export default class extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            parts: props.parts
+            fetchState: FetchStates[0].state
         }
+        this.handleDetailPageChange = this.handleDetailPageChange.bind(this)   
+    }
+
+    handleDetailPageChange (buttonValue) {
+        const newState = FetchStates.find(s => s.value === buttonValue)
+        this.setState({
+            fetchState: newState.state
+        })
     }
 
     render () {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-2"><Menu parts={this.props.parts}/></div>
-                    <div className="col-md-10"><DetailedPage /></div>
+                    <div className="col-md-2"><Menu parts={this.props.parts} onClick={this.handleDetailPageChange}/></div>
+                    <div className="col-md-10"><DetailedPage fetchState={this.state.fetchState}/></div>
                 </div>
             </div>
         )
--- front_end/js/ui/menu.js
+++ front_end/js/ui/menu.js
@@ -3,16 +3,22 @@ import React from 'react'
 export default class extends React.Component {
     constructor (props) {
         super(props)
-        this.state = {
-            parts: this.props.parts
-        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (ev) {
+        this.props.onClick(ev.target.value)
     }
 
     render () {
         return (
             <nav>
                 <ul className="nav nav-pills flex-column">
-                    {this.props.parts.map(part => <li className="nav-item" key={part.name}><button className="btn btn-link btn-block">{part.name}</button></li>)}
+                    {this.props.parts.map(part => 
+                        <li className="nav-item" key={part.name}>
+                            <button value={part.name} className="btn btn-link btn-block" onClick={this.handleChange}>{part.name}</button>
+                        </li>)
+                    }
                 </ul>
             </nav>
         )
