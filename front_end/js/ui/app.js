7d4ed7668fdefddbe386ac724443053819fe6386
MiguelNogueira <a39202@alunos.isel.pt>
2018-06-02 02:35:10 +0100

    Setup vet menu

--- front_end/js/ui/app.js
+++ front_end/js/ui/app.js
@@ -2,24 +2,27 @@ import React from 'react'
 import ClientList from './client-list'
 import GridMenu from './grid-menu'
 
-export default function (props) {  
-  const parts = [
-    {name: 'Salas de Espera', url: 'http://localhost:8081/waintingRoom'},
-    {name: 'Marcações', url: 'http://localhost:8081/appointment'},
-    {name: 'Cliente', url: 'http://localhost:8081/client'},
-    {name: 'Animal', url: 'http://localhost:8081/pet'},
-    {name: 'Produtos', url: 'http://localhost:8081/product'},
-    {name: 'Configurações', url: 'http://localhost:8081/config'}
-  ]
-  const parts2 = [
-    {name: 'Salas de Espera', url: 'http://10.10.2.56/waintingRoom'},
-    {name: 'Marcações', url: 'http://10.10.2.56/appointment'},
-    {name: 'Cliente', url: 'http://10.10.2.56/client'},
-    {name: 'Animal', url: 'http://10.10.2.56/pet'},
-    {name: 'Produtos', url: 'http://10.10.2.56/product'},
-    {name: 'Configurações', url: 'http://10.10.2.56/config'}
-  ]
+const partsRecept = [
+  {name: 'Salas de Espera', url: 'http://localhost:8081/waintingRoom'},
+  {name: 'Marcações', url: 'http://localhost:8081/appointment'},
+  {name: 'Cliente', url: 'http://localhost:8081/client'},
+  {name: 'Animal', url: 'http://localhost:8081/pet'},
+  {name: 'Produtos', url: 'http://localhost:8081/product'},
+  {name: 'Configurações', url: 'http://localhost:8081/config'}
+]
+
+const partsVet = [
+  {name: 'Consultas', url: 'http://10.10.2.56/waintingRoom'},
+  {name: 'Agenda', url: 'http://10.10.2.56/appointment'},
+  {name: 'Registo de Consultas', url: 'http://10.10.2.56/client'},
+  {name: 'Lista de Animais', url: 'http://10.10.2.56/pet'},
+  {name: 'Configurações', url: 'http://10.10.2.56/config'}
+]
+
+const isRecept = false
+
+export default function (props) {
   return (
-    <GridMenu parts={parts}/>  
+    <GridMenu parts={isRecept ? partsRecept : partsVet} isRecept={isRecept}/>
   )  
 }
