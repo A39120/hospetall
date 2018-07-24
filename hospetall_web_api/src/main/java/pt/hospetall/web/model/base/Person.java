ed6562a15af429fd0f6bb96d360c0c1656048bbe
PedroRSousa <41107@alunos.isel.ipl.pt>
2018-07-24 17:08:40 +0100

    Added error validation in Person and Client

--- hospetall_web_api/src/main/java/pt/hospetall/web/model/base/Person.java
+++ hospetall_web_api/src/main/java/pt/hospetall/web/model/base/Person.java
@@ -1,14 +1,25 @@
 package pt.hospetall.web.model.base;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @MappedSuperclass
 public abstract class Person extends BaseEntity{
 
+	@NotBlank
 	private String familyName;
+	@NotBlank
 	private String givenName;
 	private String email;
+	@NotBlank
 	private String telephone;
+
+	@NotBlank
+	@Size(min = 9, max = 9)
+	@Column(unique=true)
+	private String nif;
 
 	public String getFamilyName() {
 		return familyName;
@@ -40,5 +51,13 @@ public abstract class Person extends BaseEntity{
 
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
 	}
 }
