e24204e7e871c732088d97401d211ca7582ace9b
A39120 <lariachi@gmail.com>
2018-07-30 23:20:50 +0000

    Implementing email sender.

---
+++ hospetall_web_api/src/main/java/pt/hospetall/web/services/EmailSenderService.java
@@ -0,0 +1,32 @@
+package pt.hospetall.web.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class EmailSenderService {
+
+	private final JavaMailSender emailSender;
+
+	@Autowired
+	public EmailSenderService(@Qualifier("getJavaMailSender") JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+
+	public void sendSimpleMessage(
+			String to, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
+	}
+
+}
